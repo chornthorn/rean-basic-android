9f1b60ec4dedd602c25b4bc350841b8360ad78e7
Chorn Thron <chornthorn.kh@gmail.com>
2023-02-05 23:21:16 +0700

    first commit

---
+++ app/src/main/java/com/example/android/ConstraintHelper.kt
@@ -0,0 +1,21 @@
+package com.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.android.databinding.ActivityConstraintHelperBinding
+
+class ConstraintHelper : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConstraintHelperBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConstraintHelperBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView4.setOnClickListener {
+            binding.imageView4.visibility = View.GONE
+        }
+    }
+}
